4d60ecc50cf58d4eb4253b7d6fca77742ac6b1b1
RincLiu <i@RincLiu.com>
2015-12-14 20:53:58 +0800

    Fixed Java native method.

--- ImageProcess/Filter/Java/roid/cv/img/filter/FilterUtils.java
+++ ImageProcess/Filter/Java/roid/cv/img/filter/FilterUtils.java
@@ -1,7 +1,8 @@
 //
 // Created by rincliu on 2015/12/14.
 //
+package roid.cv.img.filter;
 
 public class FilterUtils {
-  public static native nativeGaussianBlur(byte[] imgData, int imgWidth, int imgHeight, int gaussianKernelSize) throws NullPointerException, IllegalArgumentException;
+  public static native byte[] nativeGaussianBlur(byte[] imgData, int imgWidth, int imgHeight, int gaussianKernelSize) throws NullPointerException, IllegalArgumentException;
 }
