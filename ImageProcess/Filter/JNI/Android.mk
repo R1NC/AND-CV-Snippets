e53efa001303350d31ca3f2e2445277e990c8171
RincLiu <i@RincLiu.com>
2015-12-15 19:14:24 +0800

    Modified Android.mk

--- ImageProcess/Filter/JNI/Android.mk
+++ ImageProcess/Filter/JNI/Android.mk
@@ -3,31 +3,33 @@ CURRENT_ROOT_PATH := $(LOCAL_PATH)/../../..
 OPENCV_ROOT_PATH := $(CURRENT_ROOT_PATH)/../OpenCV-Android
 OPENCV_LIBS_PATH := $(OPENCV_ROOT_PATH)/sdk/native/libs
 
-#include $(CLEAR_VARS)
+include $(CLEAR_VARS)
+
 #LOCAL_MODULE := CV_CORE
 #LOCAL_SRC_FILES := $(OPENCV_LIBS_PATH)/armeabi/libopencv_core.a
 #include $(PREBUILT_STATIC_LIBRARY)
 
-#include $(CLEAR_VARS)
 #LOCAL_MODULE := CV_IMGPROC
 #LOCAL_SRC_FILES := $(OPENCV_LIBS_PATH)/armeabi/libopencv_imgproc.a
 #include $(PREBUILT_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES := on
 OPENCV_CAMERA_MODULES := off
-OPENCV_LIB_TYPE := STATIC
+OPENCV_LIB_TYPE := SHARED
 include $(OPENCV_ROOT_PATH)/sdk/native/jni/OpenCV.mk
 
-include $(CLEAR_VARS)
 LOCAL_MODULE := FilterUtils
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/src \
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/src \
 	$(CURRENT_ROOT_PATH)/Utils \
   $(OPENCV_ROOT_PATH)/sdk/native/jni/include
-LOCAL_SRC_FILES := JNI.cxx \
+LOCAL_SRC_FILES := \
+	JNI.cxx \
 	$(CURRENT_ROOT_PATH)/Utils/JniUtils.cxx \
 	$(LOCAL_PATH)/src/FilterUtils.cxx
-#LOCAL_STATIC_LIBRARIES := CV_CORE CV_IMGPROC
+#LOCAL_STATIC_LIBRARIES := \
+	CV_CORE \
+	CV_IMGPROC
 LOCAL_CFLAGS := -std=c++11
 LOCAL_LDLIBS := -llog -ldl
 include $(BUILD_SHARED_LIBRARY)
