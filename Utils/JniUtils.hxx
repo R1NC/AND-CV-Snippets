bc953b49a6bbbb38ffbe728dcc2907a08b125a54
RincLiu <i@RincLiu.com>
2015-12-15 18:15:24 +0800

    Modified log macros.

--- Utils/JniUtils.hxx
+++ Utils/JniUtils.hxx
@@ -9,11 +9,11 @@
 #include <string>
 #include <android/log.h>
 
-#define aLogV(tag, ...) __android_log_print(ANDROID_LOG_VERBOSE, tag, __VA_ARGS__)
-#define aLogD(tag, ...) __android_log_print(ANDROID_LOG_DEBUG, tag, __VA_ARGS__)
-#define aLogI(tag, ...) __android_log_print(ANDROID_LOG_INFO, tag, __VA_ARGS__)
-#define aLogW(tag, ...) __android_log_print(ANDROID_LOG_WARN, tag, __VA_ARGS__)
-#define aLogE(tag, ...) __android_log_print(ANDROID_LOG_ERROR, tag, __VA_ARGS__)
+#define ALogV(tag, ...) __android_log_print(ANDROID_LOG_VERBOSE, tag, __VA_ARGS__)
+#define ALogD(tag, ...) __android_log_print(ANDROID_LOG_DEBUG, tag, __VA_ARGS__)
+#define ALogI(tag, ...) __android_log_print(ANDROID_LOG_INFO, tag, __VA_ARGS__)
+#define ALogW(tag, ...) __android_log_print(ANDROID_LOG_WARN, tag, __VA_ARGS__)
+#define ALogE(tag, ...) __android_log_print(ANDROID_LOG_ERROR, tag, __VA_ARGS__)
 
 
 unsigned char* jByteArrayToChars(JNIEnv *env, jbyteArray array);
