2b031996e44a4593975744d5f4aa1f4a8b8f456d
Rinc Liu <i@rincliu.com>
2015-12-16 22:43:30 +0800

    PixelsProcessor interface.

--- ImageProcess/Filter/java/roid/opencv/imgproc/filter/FilterUtils.java
+++ ImageProcess/Filter/java/roid/opencv/imgproc/filter/FilterUtils.java
@@ -7,21 +7,33 @@ import android.graphics.Bitmap;
 
 public class FilterUtils {
 
-  static {
-    System.loadLibrary("ImageProcessFilterUtils");
+  public static Bitmap gaussianBlur(Bitmap bitmap, final int gaussianKernelSize) {
+    return processBitmap(bitmap, new PixelsProcessor() {
+      @Override
+      public int[] onProcess(int[] srcPixels, int width, int height) {
+        return nativeGaussianBlur(srcPixels, width, height, gaussianKernelSize);
+      }
+    });
   }
 
-  public static Bitmap gaussianBlur(Bitmap bitmap, int gaussianKernelSize) {
-    if (bitmap != null && !bitmap.isRecycled()) {
+  private Bitmap processBitmap(Bitmap bitmap, PixelsProcessor processor) {
+    Bitmap newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+    if (bitmap != null && !bitmap.isRecycled() && processor != null) {
       final int W = bitmap.getWidth(), H = bitmap.getHeight();
       int[] srcPixels = new int[W * H];
       bitmap.getPixels(srcPixels, 0, W, 0, 0, W, H);
-      int[] dstPixels = nativeGaussianBlur(srcPixels, W, H, gaussianKernelSize);
-      Bitmap newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+      int[] dstPixels = processor.onProcess(srcPixels, W, H);
       newBitmap.setPixels(dstPixels, 0, W, 0, 0, W, H);
-      return newBitmap;
     }
-    return null;
+    return newBitmap;
+  }
+
+  private interface PixelsProcessor {
+    int[] onProcess(int[] srcPixels, int width, int height);
+  }
+
+  static {
+    System.loadLibrary("ImageProcessFilterUtils");
   }
 
   private static native int[] nativeGaussianBlur(int[] imgPixels, int imgWidth, int imgHeight, int gaussianKernelSize);
