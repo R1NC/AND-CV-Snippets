d5467edf7c90c7abf8fd8096e4de491523f7bda3
RincLiu <i@RincLiu.com>
2016-01-15 12:57:21 +0800

    auto.

--- ImageProcess/Filter/JNI/src/FilterUtils.cxx
+++ ImageProcess/Filter/JNI/src/FilterUtils.cxx
@@ -12,7 +12,7 @@
 //void boxFilter(InputArray src, OutputArray dst, int ddepth, Size ksize, Point anchor=Point(-1,-1), bool normalize=true, int borderType=BORDER_DEFAULT);
 unsigned char* boxFilter(unsigned char* imgPixels, const unsigned imgWidth, const unsigned imgHeight, unsigned kernelSize) {
   cv::Mat src(imgHeight, imgWidth, CV_8UC4, imgPixels);
-  cv::Mat dst = src.clone();
+  auto dst = src.clone();
   kernelSize = std::max(kernelSize, (unsigned)1);
   cv::Size ksize(kernelSize, kernelSize);
   int ddepth = -1;//Use src.depth().
@@ -23,7 +23,7 @@ unsigned char* boxFilter(unsigned char* imgPixels, const unsigned imgWidth, cons
 //void bilateralFilter(InputArray src, OutputArray dst, int d, double sigmaColor, double sigmaSpace, int borderType=BORDER_DEFAULT);
 unsigned char* bilateralFilter(unsigned char* imgPixels, const unsigned imgWidth, const unsigned imgHeight, unsigned kernelSize) {
   cv::Mat src(imgHeight, imgWidth, CV_8UC4, imgPixels);
-  cv::Mat dst = src.clone();
+  auto dst = src.clone();
   kernelSize = std::max(kernelSize, (unsigned)1);
   //Large filters (d > 5) are very slow, so it is recommended to use d=5 for real-time applications.
   int d = 5;
@@ -36,7 +36,7 @@ unsigned char* bilateralFilter(unsigned char* imgPixels, const unsigned imgWidth
 //void GaussianBlur(InputArray src, OutputArray dst, Size ksize, double sigmaX, double sigmaY=0, int borderType=BORDER_DEFAULT);
 unsigned char* gaussianBlur(unsigned char* imgPixels, const unsigned imgWidth, const unsigned imgHeight, unsigned kernelSize) {
   cv::Mat src(imgHeight, imgWidth, CV_8UC4, imgPixels);
-  cv::Mat dst = src.clone();
+  auto dst = src.clone();
   //The width & height of Kernel-Size must be positive and odd.
   kernelSize = std::max(kernelSize, (unsigned)1);
   if (kernelSize % 2 != 1) {
@@ -52,7 +52,7 @@ unsigned char* gaussianBlur(unsigned char* imgPixels, const unsigned imgWidth, c
 //void medianBlur(InputArray src, OutputArray dst, int ksize);
 unsigned char* medianBlur(unsigned char* imgPixels, const unsigned imgWidth, const unsigned imgHeight, unsigned kernelSize) {
   cv::Mat src(imgHeight, imgWidth, CV_8UC4, imgPixels);
-  cv::Mat dst = src.clone();
+  auto dst = src.clone();
   //The width & height of Kernel-Size must be positive and odd and greater than 1.
   kernelSize = std::max(kernelSize, (unsigned)3);
   if (kernelSize % 2 != 1) {
